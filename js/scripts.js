9f0d946ce256c133acd610164fa178414447d555
Olivia Culver <oculv21@gmail.com>
2019-08-07 19:31:40 -0400

    created search form and appended it to the search container div in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log('hello')
+/**
+ * Creates search form and appends it to the search container div
+ */
+const $searchForm = $('<form/>', {'action': '#', 'method': 'GET'});
+const $searchInput = $('<input/>', {'type': 'search', 'id': 'search-input', 'class': 'search-input', 'placeholder': 'Search...'});
+const $searchSubmit = $('<input/>', {'type': 'submit', 'value': 'Search', 'id': 'search-submit', 'class': 'search-submit'});
+
+$searchForm.append($searchInput);
+$searchForm.append($searchSubmit);
+$('.search-container').append($searchForm);
