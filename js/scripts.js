73113b34a671551d3f0749a31065459874b7129b
Olivia Culver <oculv21@gmail.com>
2019-08-11 16:10:00 -0400

    finished adding employee info to cards

--- js/scripts.js
+++ js/scripts.js
@@ -17,13 +17,27 @@ const $employeeCard =  data => {
 
   for (let i = 0; i < $employees.length; i++) {
     const employee = $employees[i];
+    console.log(employee);
 
     const $card = $('<div/>', {'class': 'card'});
     $('#gallery').append($card);
+
     const $imgContainer = $('<div/>', {'class': 'card-img-container'});
     $card.append($imgContainer);
     const $img = $('<img/>', {'class': 'card-img', 'src': `${employee.picture.large}`, 'alt': 'profile picture'});
     $imgContainer.append($img);
+
+    const $infoContainer = $('<div/>', {'class': 'card-info-container'});
+    $card.append($infoContainer);
+    const $name = $('<h3/>', {'id': 'name', 'class': 'card-name cap'});
+    $name.text(`${employee.name.first} ${employee.name.last}`);
+    $infoContainer.append($name);
+    const $email = $('<p/>', {'class': 'card-text'});
+    $email.text(`${employee.email}`);
+    $infoContainer.append($email);
+    const $location = $('<p/>', {'class': 'card-text cap'});
+    $location.text(`${employee.location.city}, ${employee.location.state}`)
+    $infoContainer.append($location);
   }
 }
 
