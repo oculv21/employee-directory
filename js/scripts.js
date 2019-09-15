56d30dbd7c4280eb90aac41f1e107db35fdafe21
Olivia Culver <oculv21@gmail.com>
2019-09-15 16:19:26 -0400

    changed styles for modal window

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 /**
  * Creates search form and appends it to the search container div
  */
-const $searchForm = $('<form/>', {'action': '#', 'method': 'GET'});
-const $searchInput = $('<input/>', {'type': 'search', 'id': 'search-input', 'class': 'search-input', 'placeholder': 'Search...'});
-const $searchSubmit = $('<input/>', {'type': 'submit', 'value': 'Search', 'id': 'search-submit', 'class': 'search-submit'});
+const $searchForm = $('<form/>', { 'action': '#', 'method': 'GET' });
+const $searchInput = $('<input/>', { 'type': 'search', 'id': 'search-input', 'class': 'search-input', 'placeholder': 'Search...' });
+const $searchSubmit = $('<input/>', { 'type': 'submit', 'value': 'Search', 'id': 'search-submit', 'class': 'search-submit' });
 
 $searchForm.append($searchInput);
 $searchForm.append($searchSubmit);
@@ -17,7 +17,7 @@ const createCard = employee => {
   const employeeImg = employee.picture.large;
   const employeeName = `${employee.name.first} ${employee.name.last}`;
   const employeeEmail = employee.email;
-  const $card = $('<div/>', {'class': 'card'});
+  const $card = $('<div/>', { 'class': 'card' });
   $card.html(`
     <div class="card-img-container">
       <img class="card-img" src=${employeeImg} alt="profile picture">
@@ -27,7 +27,7 @@ const createCard = employee => {
       <p class="card-text">${employeeEmail}</p>
       <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
     </div>`);
-    $('#gallery').append($card);
+  $('#gallery').append($card);
 }
 
 /**
@@ -39,7 +39,7 @@ const createModal = (employee, i) => {
   const employeeImg = employee.picture.large;
   const employeeName = `${employee.name.first} ${employee.name.last}`;
   const employeeEmail = employee.email;
-  const $modalContainer = $('<div/>', {'class': 'modal-container'});
+  const $modalContainer = $('<div/>', { 'class': 'modal-container' });
   $modalContainer.html(`
     <div class="modal">
       <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
@@ -58,11 +58,11 @@ const createModal = (employee, i) => {
   $modalContainer.hide();
 
   //click event listeners for opening and closing modal window
-  employeeCard.click(function() {
+  employeeCard.click(function () {
     $modalContainer.show();
   });
   const closeBtn = $('.modal-close-btn');
-  closeBtn.click(function() {
+  closeBtn.click(function () {
     $modalContainer.hide()
   });
 }
@@ -73,23 +73,23 @@ let employeeNames = []; //array to be filled with employee names for use by sear
  * Loops throught the employees to create a card and modal window for each
  * @param {data}  
  */
-const employeeInfo =  data => {
+const employeeInfo = data => {
   const employees = data.results;
-  
+
   for (let i = 0; i < employees.length; i++) {
     const employee = employees[i];
     const name = `${employee.name.first} ${employee.name.last}`;
     employeeNames.push(name);
     createCard(employee);
-    createModal(employee, i); 
-  } 
+    createModal(employee, i);
+  }
 }
 
 /**
  * Allows user to search through employees using search bar
  * @param {array} employee names to search through
  *  
- */ 
+ */
 const searchEmployees = names => {
   names.forEach(name => {
     let i = names.indexOf(name);
@@ -115,10 +115,9 @@ $searchForm.submit(() => {
  * API request to generate 12 random employees
  */
 $.ajax({
-    url: 'https://randomuser.me/api/?results=12&nat=us,gb,au,ca',
-    dataType: 'json',
-    success: function (data) {
-      employeeInfo(data)
-    }
-  });
-
+  url: 'https://randomuser.me/api/?results=12&nat=us,gb,au,ca',
+  dataType: 'json',
+  success: function (data) {
+    employeeInfo(data)
+  }
+});
